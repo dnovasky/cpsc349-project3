3aa0e82e2c9bc06d7e1e161b416f169de1a80485
Donald Novasky <dnovasky@hotmail.com>
2022-12-01 02:09:37 -0800

    Added function to determine the winner of a match and update score accordingly.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,60 @@
-var yourChoice;
-var yourScore = 0;
-var opponentChoice;
-var opponentScore = 0;
-var choices = ['rock', 'paper', 'scissors'];
-window.onload = start;
+let yourChoice
+let player1 = 0
+let opponentChoice
+let player2 = 0
+const choices = ['rock', 'paper', 'scissors']
+window.onload = start
 
 function start() {
-    for (let i = 0; i < 3; i++) {
-        const choice = document.createElement('img');
-        choice.id = choices[i];
-        choice.src = choices[i] + '.png';
-        document.getElementById('choices').append(choice);
-        choice.addEventListener('click', selectChoice);
-    }
+  for (let i = 0; i < 3; i++) {
+    const choice = document.createElement('img')
+    choice.id = choices[i]
+    choice.src = choices[i] + '.png'
+    document.getElementById('choices').append(choice)
+    choice.addEventListener('click', selectChoice)
+  }
 }
 
 function selectChoice() {
-    yourChoice = this.id;
-    document.getElementById('yourChoice').src = yourChoice + '.png';
+  yourChoice = this.id
+  document.getElementById('yourChoice').src = yourChoice + '.png'
 
-    opponentChoice = choices[Math.floor(Math.random() * 3)];
-    document.getElementById('opponentChoice').src = opponentChoice  + '.png';
+  opponentChoice = choices[Math.floor(Math.random() * 3)]
+  document.getElementById('opponentChoice').src = opponentChoice + '.png'
+
+  const winner = selectWinner(yourChoice, opponentChoice)
+  if (winner) {
+    updateScore(winner)
+  }
+}
+
+function selectWinner(yourChoice, opponentChoice) {
+  let result = yourChoice + opponentChoice
+  let winner = ''
+  switch (result) {
+    // case 'rockrock', 'paperpaper', 'scissorsscissors':
+    //   winner = 'tie'
+    case 'rockscissors':
+    case 'paperrock':
+    case 'scissorspaper':
+      winner = 'Player1'
+      break
+    case 'rockpaper':
+    case 'paperscissors':
+    case 'scissorsrock':
+      winner = 'Player2'
+  }
+  return winner
+}
+
+function updateScore(winner) {
+  if (winner === 'Player1') {
+    player1 += 1
+    document.querySelector('#yourScore').textContent = `Your Score: ${player1}`
+  } else {
+    player2 += 1
+    document.querySelector(
+      '#opponentScore'
+    ).textContent = `Opponent's Score: ${player2}`
+  }
 }
