6dd74e59ee80ab86bbb758d279cd4e7b1c21cd74
Donald Novasky <dnovasky@hotmail.com>
2022-12-01 21:06:30 -0800

    Added button effects when choosing RPS. Moved choice buttons over to player side.

--- js/index.js
+++ js/index.js
@@ -13,6 +13,12 @@ function start() {
   initModal()
   for (let i = 0; i < 3; i++) {
     const choice = document.createElement('img')
+    choice.classList.add(
+      'rounded-full',
+      'hover:shadow-2xl',
+      'active:ring',
+      'active:ring-green-700'
+    )
     choice.id = choices[i]
     choice.src = choices[i] + '.png'
     document.getElementById('choices').append(choice)
@@ -56,12 +62,12 @@ function updateScore(winner) {
   let playerTwo = document.querySelector('#opponentScore')
   if (winner === 'Player1') {
     player1 += 1
-    playerOne.textContent = `Your Score: ${player1}`
+    playerOne.textContent = `You: ${player1}`
     playerOne.style.backgroundColor = 'green'
     playerTwo.style.backgroundColor = 'red'
   } else if (winner === 'Player2') {
     player2 += 1
-    playerTwo.textContent = `Opponent's Score: ${player2}`
+    playerTwo.textContent = `Opponent: ${player2}`
     playerTwo.style.backgroundColor = 'green'
     playerOne.style.backgroundColor = 'red'
   } else {
